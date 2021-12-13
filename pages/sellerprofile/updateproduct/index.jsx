f46993ef5df129ad609dc01ab29369168a248b17
Trace White <traceannwhite@gmail.com>
2021-12-13 09:13:12 -0600

    seller profile deleted

--- pages/sellerprofile/updateproduct/index.jsx
+++
@@ -1,7 +0,0 @@
-// import ProductInfoForm from "../../../components/ProductInfoForm";
-
-const UpdateProductPage = () => {
-  return <div>{/* <ProductInfoForm /> */}</div>;
-};
-
-export default UpdateProductPage;
